5e45a7b3f81fdfb554fd25a2826cedc8c2c355ad
Xindi Wu <wuxindi0709@gmail.com>
2020-12-08 16:38:07 -0500

    Handle more edge cases in batch processing

--- tools/write_video_app.py
+++ tools/write_video_app.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
     for subfile in os.listdir(args.base_dir):
         command = f"{base_command} --base_path '{args.base_dir}/{subfile}' --target_path '../../{subfile[:-4]}/'"
         try:
-            if "result.mp4" not in os.listdir(f'../../{subfile[:-4]}/'):
+            if os.path.isdir(f'{args.base_dir}/{subfile}') or not os.path.isdir(f'../../{subfile[:-4]}/'):
+                continue
+            if 'mp4' in subfile.lower() and "result.mp4" not in os.listdir(f'../../{subfile[:-4]}/'):
                 print(command)
                 os.system(command)
         except Exception as e:
