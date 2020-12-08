9b4e9de21488a7e2189bf562083dd9ebe66620e9
Xindi Wu <wuxindi0709@gmail.com>
2020-12-08 16:39:11 -0500

    Handle more edge cases in batch processing, and default resume

--- tools/clean_video_app.py
+++ tools/clean_video_app.py
@@ -3,7 +3,7 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Anonymize a batch of videos')
-parser.add_argument('--resume', default='SiamMask_DAVIS.pth', type=str, required=True,
+parser.add_argument('--resume', default='SiamMask_DAVIS.pth', type=str,
                     metavar='PATH',help='path to latest checkpoint (default: none)')
 parser.add_argument('--config', dest='config', default='config_davis.json',
                     help='hyper-parameter of SiamMask in json format')
@@ -13,6 +13,7 @@ args = parser.parse_args()
 if __name__ == '__main__':
     base_command = f"python ../../tools/video_cleaner.py --resume {args.resume} --config {args.config}"
     for subfile in os.listdir(args.base_dir):
-        command = f"{base_command} --base_path '{args.base_dir}/{subfile}' --target_path '../../{subfile[:-4]}/'"
-        print(command)
-        os.system(command)
+        if "mp4" in subfile.lower():
+            command = f"{base_command} --base_path '{args.base_dir}/{subfile}' --target_path '../../{subfile[:-4]}/'"
+            print(command)
+            os.system(command)
