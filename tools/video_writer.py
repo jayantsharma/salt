1dfd31431863c9b83e1a0c219a737d4ffb1933d8
Eric Zelikman <ezelikman@stanford.edu>
2020-12-12 01:59:27 -0500

    Updated video writer to load npz files instead of images

--- tools/video_writer.py
+++ tools/video_writer.py
@@ -12,8 +12,9 @@ import os
 import time
 
 parser = argparse.ArgumentParser(description='PyTorch Tracking Demo')
-parser.add_argument('--base_path', default='../../data/tennis', help='datasets')
-parser.add_argument('--target_path', default='../../results/', help='datasets')
+parser.add_argument('--base_path', default='../../data/tennis', help='original video location')
+parser.add_argument('--target_path', default='../../results/', help='save target folder path')
+parser.add_argument('--video_name', default='result.mp4', help='resulting video name')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -22,15 +23,17 @@ if __name__ == '__main__':
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    out = cv2.VideoWriter(args.target_path + "result.mp4", fourcc, fps, (width, height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(args.target_path + args.video_name, fourcc, fps, (width, height), True)
     f = 0
     start_time = time.time()
     ret, im = cap.read()
     while ret:
-        if f % (fps) == 0: print(f // fps)
-        if str(f) + ".png" in os.listdir(args.target_path):
-            im = cv2.imread(args.target_path + str(f) + ".png")
+        if f % (fps) == 0: print(f'{f // fps} / {length // fps}')
+        if str(f) + ".npz" in os.listdir(args.target_path):
+            mask = np.load(args.target_path + str(f) + ".npz")["arr_0"]
+            blur = cv2.blur(im, (30, 30), cv2.BORDER_DEFAULT)
+            im = (mask == 0)[:, :, None] * blur + (mask != 0)[:, :, None] * im
         out.write(im)
         ret, im = cap.read()
         f += 1
