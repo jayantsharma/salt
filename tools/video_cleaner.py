840a690ad30b72cf5dfd017fe043cdd3b2b36fd1
Eric Zelikman <ezelikman@stanford.edu>
2020-12-12 01:58:34 -0500

    Added a video trackbar, reduced saved size by >99.9%, background video export option

--- tools/video_cleaner.py
+++ tools/video_cleaner.py
@@ -6,10 +6,13 @@
 # Written by Qiang Wang (wangqiang2015 at ia.ac.cn)
 # --------------------------------------------------------
 import glob
-from test import *
 import os
 import json
+import subprocess
+import time
+from test import *
 from concurrent.futures import ProcessPoolExecutor as Executor
+from distutils.util import strtobool
 
 parser = argparse.ArgumentParser(description='PyTorch Tracking Demo')
 
@@ -49,7 +52,59 @@ def write_metadata(metadata_path, base_path, metadata):
     with open(metadata_path, "w") as cur_file:
         json.dump(old_metadata, cur_file)
 
+class VideoPlayer():
+    def __init__(self):
+        self.f = 0
+        self.manual_update = True
+        self.cap = cv2.VideoCapture(args.base_path)
+        cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+        self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame = self.cap.read()
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.trackbar_name = "Current Frame"
+        cv2.createTrackbar(self.trackbar_name, window_name, 0, self.length, self.set_frame)
 
+        self.get_new_example = False
+        self.mask_enabled = False
+
+    def get_cur_frame(self):
+        return self.frame
+
+    def set_frame(self, new_f):
+        if self.manual_update:
+            self.f = min(max(new_f, 0), self.length)
+            self.cap.set(1, self.f)
+    
+    def shift_frame(self, delta):
+        self.set_frame(self.f + delta)
+    
+    def next_frame(self):
+        self.f += 1
+        self.manual_update = False
+        cv2.setTrackbarPos(self.trackbar_name, window_name, self.f)
+        self.manual_update = True
+        self.frame = self.cap.read()
+        return self.frame
+
+    def end(self):
+        self.cap.release()
+        cv2.destroyWindow(window_name)
+
+    def enter_mask_mode(self):
+        self.mask_enabled = True
+        self.get_new_example = True
+
+def confirm(prompt, default=None):
+    while True:
+        answer = input(prompt)
+        if len(answer) == 0 and default is not None:
+            return default
+        else:
+            try:
+                return bool(strtobool(answer))
+            except:
+                print("Please choose a valid option")
+                continue
 
 if __name__ == '__main__':
     if not os.path.exists(args.target_path):
@@ -73,17 +128,14 @@ if __name__ == '__main__':
 
     base_siammask = get_siammask()
 
-    # Parse Image file
-    cap = cv2.VideoCapture(args.base_path)
-    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-    toc, f = 0, 0
-    ret, im = cap.read()
+    target_value = 0
+    toc = 0
+    vid = VideoPlayer()
+    ret, im = vid.get_cur_frame()
 
     key = cv2.waitKey(0)
-    mask_enabled = key != 99
-    get_new_example = mask_enabled
+    vid.mask_enabled = key != 99
+    vid.get_new_example = vid.mask_enabled
     states = []
     metadata = {}
     cur_identity = get_max_identity(args.metadata_path)
@@ -91,7 +143,7 @@ if __name__ == '__main__':
     while ret:
         tic = cv2.getTickCount()
         # If in selection mode
-        if get_new_example and mask_enabled:  # init
+        if vid.get_new_example and vid.mask_enabled:  # init
             cur_identity += len(states) # Count the previous boxes
             states = []
             init_rects = cv2.selectROIs(window_name, im, False, False)
@@ -104,55 +156,53 @@ if __name__ == '__main__':
                     target_pos = np.array([x + w / 2, y + h / 2])
                     target_sz = np.array([w, h])
                     states.append(siamese_init(im, target_pos, target_sz, siammask, cfg['hp'], device=device))  # init tracker
-                get_new_example = False
+                vid.get_new_example = False
             
             key = cv2.waitKey(0)
             print(key)
-            if key == 97:
-                f -= 12
-                cap.set(1, f)
-            elif key == 100:
-                f += 12
-                cap.set(1, f)
-            elif key == 115:
-                mask_enabled = False
-        elif f > 0:  # tracking
+            if key == 97: vid.shift_frame(-12)
+            elif key == 100: vid.shift_frame(12)
+            elif key == 115: vid.mask_enabled = False
+        else: # tracking
             # If you're actually tracking the objects
-            if mask_enabled:
+            if vid.mask_enabled:
                 # Initiate the frame metadata
-                metadata[f] = {'index': f, 'tracked_objects': []}
+                metadata[vid.f] = {'index': vid.f, 'tracked_objects': []}
+                all_mask = np.ones_like(im)[:, :, 0]
                 for state_idx, state in enumerate(states):
                     states[state_idx] = siamese_track(state, im, mask_enable=True, refine_enable=True, device=device)  # track
                     # Add the object metadata to the list of tracked objects for this frame
-                    metadata[f]['tracked_objects'].append({
+                    metadata[vid.f]['tracked_objects'].append({
                         "boundingBox": state['ploygon'].tolist(),
                         "identity": cur_identity + state_idx, # Keep a different identity for each mask
                         "score": state["score"].item()
                     })
                     location = state['ploygon'].flatten()
-                    mask = state['mask'] > state['p'].seg_thr * 0.9
-                    blur = cv2.blur(im, (30, 30), cv2.BORDER_DEFAULT)
-                    im = (mask > 0)[:, :, None] * blur + (mask == 0)[:, :, None] * im
-                    write_executor.submit(cv2.imwrite, args.target_path + str(f) + ".png", im, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+                    mask = state['mask'] < state['p'].seg_thr
+                    all_mask *= mask
+                blur = cv2.blur(im, (30, 30), cv2.BORDER_DEFAULT)
+                im = (all_mask == 0)[:, :, None] * blur + (all_mask != 0)[:, :, None] * im
+                write_executor.submit(np.savez_compressed, args.target_path + str(vid.f) + ".npz", all_mask > 0)
             cv2.imshow(window_name, im)
             key = cv2.waitKey(1)
             if key > 0:
-                if key == 97:
-                    f -= 48
-                    cap.set(1, f)
-                elif key == 100:
-                    f += 48
-                    cap.set(1, f)
+                if key == 97: vid.shift_frame(-48)
+                elif key == 100: vid.shift_frame(48)
                 else:
-                    mask_enabled = key != 99
-                    get_new_example = mask_enabled
+                    vid.mask_enabled = key != 99
+                    vid.get_new_example = vid.mask_enabled
                 
         toc += cv2.getTickCount() - tic
-        ret, im = cap.read()
-        f += 1
+        ret, im = vid.next_frame()
     if args.metadata_path is not None:
         write_metadata(args.metadata_path, args.base_path, metadata)
     toc /= cv2.getTickFrequency()
-    fps = f / toc
-    cap.release()
+    fps = vid.length / toc
+    vid.end()
     print(f'{window_name} Time: {toc:02.1f}s Speed: {fps:3.1f}fps (with visulization!)')
+    if confirm('Would you like to export the video? [Y/n] ', default=True):
+        if os.path.exists(args.target_path + args.base_path.split("/")[-1]):
+            if not confirm("WARNING: File already exists. Are you sure you want to overwrite the video? "):
+                quit()
+        print("Exporting video in the background!")
+        subprocess.Popen(['python', '../../tools/video_writer.py', '--base_path', args.base_path, '--target_path', args.target_path, "--video_name", args.base_path.split("/")[-1]])
